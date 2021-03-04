6e9a8ab7896ec95acf773b9192e5f96e124f2b0c
Steffen Suess <kontakt@steffensuess.de>
2021-03-04 17:40:12 +0100

    -finished Part1

--- lib/main.dart
+++ lib/main.dart
@@ -34,25 +34,79 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title!),
       ),
-      body: Center(
-        child: ListView(
-          children: [
-            for (var person in people)
-              ListTile(
-                leading: Image.network(person.picture),
-                title: Text(person.name),
-                onTap: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => Scaffold(
-                      appBar: AppBar(),
-                      body: PersonDetail(person),
-                    ),
-                  ),
-                ),
-              ),
-          ],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 600) {
+            return WideLayout();
+          } else {
+            return NarrowLayout();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class WideLayout extends StatefulWidget {
+  @override
+  _WideLayoutState createState() => _WideLayoutState();
+}
+
+class _WideLayoutState extends State<WideLayout> {
+  Person? _person;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          child: PeopleList(onPersonTap: (person) {
+            setState(() {
+              _person = person;
+            });
+          }),
+          flex: 2,
+        ),
+        Flexible(
+          child: _person == null ? Placeholder() : PersonDetail(_person!),
+          flex: 3,
+        ),
+      ],
+    );
+  }
+}
+
+class NarrowLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: PeopleList(
+        onPersonTap: (person) => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => Scaffold(
+              appBar: AppBar(),
+              body: PersonDetail(person),
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class PeopleList extends StatelessWidget {
+  final void Function(Person) onPersonTap;
+  const PeopleList({required this.onPersonTap});
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        for (var person in people)
+          ListTile(
+            leading: Image.network(person.picture),
+            title: Text(person.name),
+            onTap: () => onPersonTap(person),
+          ),
+      ],
     );
   }
 }
